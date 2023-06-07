49665137deab840cbef5d105f7e96c977c737a08
taddeoneri <taddeo.neri10@gmail.com>
2023-06-07 15:25:00 +0200

    views

---
+++ config/db.php
@@ -0,0 +1,46 @@
+<?php
+return [
+    'projects' => [
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:ZDEzODQyM2QtNDhiZS00:ZWRhZTlj/pag_8_fat_peolple_don-t_go_to_heaven-def-jpg.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Il paradiso è dei magri",
+            "description" => "A partire da radici antiche la condanna della gola ha avuto una particolare rilevanza nella definizione della buona cristiana e del buon cristiano.<br>Tale condanna si ripete ancora oggi, attraverso l’ossessione per il corpo magro, la colpevolizzazione dell’obesità o anche del semplice sovrappeso.<br>Un punto di osservazione giudicante mette malamente in discussione le persone che soffrono di disturbi alimentari e racconta una storia che ancora oggi fa risuonare la propria eco, soprattutto nelle comunità statunitensi."
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:MWU2ZmEyZWEtMDI5MS00:ODAzZjEy/obj92908007_1.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Radio, podcast e digitale al posto della televisione. La cultura si frammenta",
+            "description" => "Per capire a colpo d’occhio quanto la televisione sia cambiata basta un confronto degli argomenti che a partire dal 2012 hanno offerto la materia al Festival della Tv, che occupa annualmente le piazze di Dogliani nel cuore delle Langhe. Nella prima edizione, tra gli scricchiolii della crisi finanziaria e con i tecnici al governo, la parte principale del proscenio era presa da manager e volti del duopolio Rai-Mediaset, entrambe impegnate, anche dietro l’incalzare di La7, a suonare il piffero anti casta. Argomento allora obbligato, astuto e taumaturgico per radunare pubblico ai talk show."
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:N2U1M2IxNWUtMjEyNi00:OWJhZjc5/rapito_di_marco_bellocchio_ph-anna-camerlingo_dsc5099_-jpg.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Bellocchio racconta la storia che Spielberg sognava",
+            "description" => "Rapito, in concorso a Cannes, è l’ultimo film di Marco Bellocchio ed esce il 25 maggio in Italia: una requisitoria feroce contro i delitti in nome di Dio.<br>Il sequestro di Edoardo Mortara nel 1858, bambino ebreo da educare alla fede cattolica, riporta il regista de L’ora di religione e de I pugni in tasca a uno dei suoi temi più cari.<br>Chiarissima la lettura politica che Bellocchio dà della vicenda: «Rappresenta la volontà disperata, e perciò violentissima, di un’autorità ormai agonizzante di resistere al suo crollo, anzi di contrattaccare»."
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:OWE2MzI0YmYtYjA5My00:ZGVlYzhh/obj92909776_1.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Il mistero degli Nft visto da un boomer dell’arte",
+            "description" => "Gli Nft (Non Fungible Token) rappresentano una rivoluzione tecnologica che permette la vendita, l'acquisto e lo scambio di oggetti virtuali tramite informazioni digitali certificate su una blockchain.<br>Gli artisti hanno compreso le potenzialità degli Nft, rompendo le tradizionali visioni dell'arte e aprendo nuove possibilità di promozione e vendita autonoma.<br>Le generazioni Y e Z sono pronte ad abbracciare questo nuovo modo di comunicare, scambiare economie e necessità, mentre le opere virtuali sfidano l'idea tradizionale di possesso fisico e ridefiniscono il collezionismo."
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:NWI5YTgzZmQtNWM5NC00:MzQ1OWUy/san-francisco-chronicle-archive-image.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Il visore è il nuovo smartphone. Apple punta sulla realtà aumentata",
+            "description" => "Pochi utenti, improvvisi cambi di strategia e ingenti perdite, a distanza di un anno e mezzo dalla presentazione il metaverso di Mark Zuckerberg non sta affatto andando per il verso giusto<br>Anche per questo, Meta sembra star puntando su una tecnologia per molti versi opposta alla realtà virtuale, vale a dire la realtà aumentata, che sovrappone elementi digitali al mondo fisico<br>Su questo fronte, Apple è però in netto vantaggio e dovrebbe presentare a breve il suo primo visore dedicato proprio alla realtà aumentata, che potrebbe segnare un importante momento di passaggio"
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:YmJjNmRiMzctNzJlMS00:YjZhOGM5/emirates-elon-musk.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Starship esplode dopo 4 minuti. Ma per SpaceX è un successo",
+            "description" => "La compagnia di Elon Musk SpaceX ha effettuato il test di volo del razzo Starship, previsto per lo scorso 17 aprile ma posticipato a giovedì per problemi tecnici. Il lancio si è interrotto dopo pochi minuti con l’esplosione di Starship. Musk e la sua società hanno commentato positivamente i risultati ottenuti, comunicando di star già lavorando a migliorie in vista del successivo test, previsto nei prossimi mesi."
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:ZjcwZjYxMGYtNjdiMi00:MGExMjE4/11590257.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Lo scrittore ucciso verso sera dalle bugie delle sue lettrici",
+            "description" => "Un grande scrittore a fine vita viene preso da uno scrupolo assurdo: e se nessuno lo avesse mai letto?<br>Nella sua stanza d’ospedale – dove transitano infermieri, editori, amanti e uffici stampa – attua una messinscena per smascherare gli impostori dai suoi lettori veri<br>L’ossessione del grande scrittore malato è una storia che nasconde una preoccupazione reale: la perdita di centralità della letteratura, in un mondo social in cui conta non tanto leggere quanto dichiarare di aver letto"
+        ],
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:ZGQ2MDg4OWMtZWQxOS00:Y2RiMWNi/britain-the-little-mermaid-arrivals.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Non ci sono più le principesse di una volta. La Sirenetta nera è una ricchezza",
+            "description" => "Le sirene sono diventate un simbolo della comunità trans e non binaria. Parlano di un’identità doppia, un’appartenenza ibrida.<br>È più che sensato che una storia incentrata sul desiderio di diventare altro intercetti la riscrittura anche di altri stereotipi, come quelli razziali.<br>Non è escluso ci possano essere eccessi strumentali o che il capitalismo sfrutti i temi della diversity. Ma tale è il debito che abbiamo verso le forme di umanità escluse e umiliate che un eventuale, momentaneo, sbilanciamento al contrario non ci farà poi così male."
+        ]
+
+    ]
+];
